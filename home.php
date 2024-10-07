5d33e6ebc7238aed9ae4a89a516b23a2251a92fa
Gabriel Carmo <gabrielcarmo1602@gmail.com>
2024-10-07 10:21:30 -0300

    1st commit

---
+++ home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coração solidario HOME</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <a href="#">
+                <img src="assets/logo coracao.png" alt="Logo Coração Solidário">
+                <h1>
+                    Coração Solidário
+                </h1>
+            </a>           
+        </div>
+        <ul>
+            <li>
+                <a href="#">Masculino</a>
+            </li>
+            <li>
+                <a href="#">Feminino</a>
+            </li>
+            <li>
+                <a href="#">Infantil</a>
+            </li>
+            <li>
+                <a href="#">Calçados</a>
+            </li>
+            <li>
+                <a href="#">Sobre Nós</a>
+            </li>
+            <li>
+                <a href="#">Entrar</a>
+            </li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
