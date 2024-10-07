9c9f9a8cfbb4c1b772b9ad7d6809432ee344ef86
marcel araujo <marcelaraujo1440@gmail.com>
2024-10-07 18:59:32 -0300

    arrumei ia

--- teste.php
+++ teste.php
@@ -8,8 +8,8 @@
 <body>
     <header>
         <div class="logo">
-            <img src="logo.png" alt="Coração Solidário">
-            <span>Coração Solidário</span>
+            <img class="logo-1" src="assets/logo coracao.png" alt="Coração Solidário">
+            <a>Coração Solidário</a>
         </div>
         <nav>
             <ul>
@@ -24,12 +24,13 @@
             <button>Entrar</button>
         </div>
     </header>
-    <h1>Masculino/Feminino/Infantil/Calçados</h1>
-
     <div class="cart-icon">
-        <img src="shopping-cart.png" alt="Carrinho de compras">
+        <a href="#"><img src="assets/shopping-cart.png" alt="Carrinho de compras"></a>
     </div>
 
+    <h1>Coração Solidário</h1>
+
+    
     <main>
         <section class="products">
             <div class="product">
@@ -58,6 +59,28 @@
                 <p>Text</p>
             </div>
         </section>
-
+    </main>
+        <footer>
+        <div class="footer-content">
+            <div class="footer-section">
+                <h4>Redes sociais</h4>
+                <div class="social-icons">
+                    <img src="assets/instagram.png" alt="Instagram">
+                    <img src="assets/twitter.png" alt="X (Twitter)">
+                </div>
+            </div>
+            <div class="footer-section">
+                <h4>Email de contato</h4>
+                <p>coracao.solidario@gmail.com</p>
+            </div>
+            <div class="footer-section">
+                <h4>Telefone</h4>
+                <p>41 99999-9999</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>© Todos os direitos reservados | coração solidário | 2024</p>
+        </div>
+    </footer>
 </body>
 </html>
