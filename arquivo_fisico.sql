977d65ebd78da97c8cd1f9e88512aa91fffb2eeb
IgorThiagoLara <163324764+IgorThiagoLara@users.noreply.github.com>
2024-10-23 08:10:36 -0300

    Add files via upload

---
+++ arquivo_fisico.sql
@@ -0,0 +1,95 @@
+DROP DATABASE brecho;
+CREATE DATABASE brecho;
+USE brecho;
+
+DROP TABLE IF EXISTS Cliente;
+DROP TABLE IF EXISTS Doador;
+DROP TABLE IF EXISTS Cadastro;
+DROP TABLE IF EXISTS Produto;
+DROP TABLE IF EXISTS Enetrega;
+DROP TABLE IF EXISTS Roupa;	
+DROP TABLE IF EXISTS Calcado;
+DROP TABLE IF EXISTS Imagem;
+DROP TABLE IF EXISTS Donatario;
+DROP TABLE IF EXISTS Administrador_site;
+DROP TABLE IF EXISTS Usuario_cadastrado;
+
+CREATE TABLE Usuario_geral(
+	CPF  CHAR(11) PRIMARY KEY NOT NULL UNIQUE,
+    nome_completo VARCHAR(100) NOT NULL,
+    Data_nascimento DATE NOT NULL,
+    Telefone VARCHAR(11) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Administrador_site(
+	Usuario_admin VARCHAR(20) PRIMARY KEY NOT NULL UNIQUE,
+    Senha_admin VARCHAR(15) NOT NULL UNIQUE
+);
+
+CREATE TABLE Cliente (
+	Usuario_cliente VARCHAR(20) PRIMARY KEY NOT NULL UNIQUE,
+    Senha_cliente VARCHAR(15) NOT NULL UNIQUE
+);
+
+CREATE TABLE Doador (
+	ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Rua VARCHAR(50) NOT NULL,
+    Bairro VARCHAR(19) NOT NULL,
+    Numero INT,
+    Complemento VARCHAR(50)
+);
+
+CREATE TABLE Donatario (
+	ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT
+);
+
+CREATE TABLE Produto (
+    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Nome VARCHAR(50) NOT NULL,
+    Categoria VARCHAR(7) NOT NULL,
+    Publico_alvo VARCHAR(9) NOT NULL,
+    Data_postagem DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    Descricao VARCHAR(200),
+    Usuario_admin VARCHAR(20) NOT NULL,
+    FOREIGN KEY (Usuario_admin) REFERENCES Administrador_site(Usuario_admin)
+);
+
+CREATE TABLE Cadastro (
+	ID_doador INT NOT NULL,
+    ID_produto INT NOT NULL,
+    FOREIGN KEY(ID_doador) REFERENCES Doador(ID),
+    FOREIGN KEY(ID_produto) REFERENCES Produto(ID)
+);
+
+CREATE TABLE Entrega(
+	ID_donatario INT NOT NULL,
+    ID_produto INT NOT NULL,
+    Status VARCHAR(12) NOT NULL,
+    Data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+    FOREIGN KEY(ID_donatario) REFERENCES Donatario(ID),
+    FOREIGN KEY(ID_produto) REFERENCES Produto(ID)
+);
+
+CREATE TABLE Roupa(
+	ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Tamanho_roupa VARCHAR(3) NOT NULL,
+    Cor_roupa VARCHAR(19) NOT NULL,
+    ID_produto INT NOT NULL,
+    FOREIGN KEY(ID_produto) REFERENCES Produto(ID)
+);
+
+CREATE TABLE Calcado (
+	ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Tamanho_calcado VARCHAR(3) NOT NULL,
+    Cor_calcado VARCHAR(19) NOT NULL,
+    ID_produto INT NOT NULL,
+    FOREIGN KEY(ID_produto) REFERENCES Produto(ID)
+);
+
+CREATE TABLE Imagem (
+	ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Caminho_imagem VARCHAR(100) NOT NULL,
+    ID_produto INT NOT NULL,
+    FOREIGN KEY(ID_produto) REFERENCES Produto(ID)
+);
