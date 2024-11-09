22812b473aea76bf95966adb9ea1d9e5767d317b
IgorThiagoLara <163324764+IgorThiagoLara@users.noreply.github.com>
2024-11-08 22:45:49 -0300

    Update arquivo_fisico.sql

--- arquivo_fisico.sql
+++ arquivo_fisico.sql
@@ -3,17 +3,20 @@ CREATE DATABASE brecho;
 USE brecho;
 
 DROP TABLE IF EXISTS Cliente;
-DROP TABLE IF EXISTS Cadastro_produto;
+DROP TABLE IF EXISTS Doador;
+DROP TABLE IF EXISTS Cadastro;
 DROP TABLE IF EXISTS Produto;
-DROP TABLE IF EXISTS Entrega_produto;
+DROP TABLE IF EXISTS Enetrega;
 DROP TABLE IF EXISTS Roupa;	
 DROP TABLE IF EXISTS Calcado;
 DROP TABLE IF EXISTS Imagem;
+DROP TABLE IF EXISTS Donatario;
 DROP TABLE IF EXISTS Administrador_site;
 DROP TABLE IF EXISTS Usuario_cadastrado;
 
 CREATE TABLE Usuario_geral(
-	CPF  CHAR(11) PRIMARY KEY NOT NULL UNIQUE,
+	ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	CPF  CHAR(11) NOT NULL UNIQUE,
     Nome_completo VARCHAR(100) NOT NULL,
     Data_nascimento DATE NOT NULL,
     Telefone VARCHAR(11) NOT NULL,
@@ -29,10 +32,12 @@ CREATE TABLE Cliente (
 	ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	Usuario_cliente VARCHAR(20) NOT NULL UNIQUE,
     Senha_cliente VARCHAR(15) NOT NULL UNIQUE,
-    Rua VARCHAR(50),
+    Logradouro VARCHAR(50),
     Bairro VARCHAR(19),
     Numero INT,
-    Tipo_cliente VARCHAR(9)
+    Tipo_cliente VARCHAR(9),
+    ID_usuario_geral INT,
+    FOREIGN KEY (ID_usuario_geral) REFERENCES Usuario_geral(ID)
 );
 
 CREATE TABLE Produto (
@@ -48,18 +53,18 @@ CREATE TABLE Produto (
 );
 
 CREATE TABLE Cadastro_produto (
-	ID_doador INT NOT NULL,
-    ID_produto INT NOT NULL,
-    FOREIGN KEY(ID_doador) REFERENCES Doador(ID),
+	ID_cliente INT,
+    ID_produto INT,
+    FOREIGN KEY(ID_cliente) REFERENCES Cliente(ID),
     FOREIGN KEY(ID_produto) REFERENCES Produto(ID)
 );
 
-CREATE TABLE Entrega_produto(
-	ID_donatario INT NOT NULL,
+CREATE TABLE Entrega(
+	ID_cliente INT NOT NULL,
     ID_produto INT NOT NULL,
-    Status VARCHAR(12) NOT NULL,
+    Status_pedido VARCHAR(12) NOT NULL,
     Data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
-    FOREIGN KEY(ID_donatario) REFERENCES Donatario(ID),
+    FOREIGN KEY(ID_cliente) REFERENCES Cliente(ID),
     FOREIGN KEY(ID_produto) REFERENCES Produto(ID)
 );
 
