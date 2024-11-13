11b66f0f436be29853fc4c83142761cbe1b94482
marcelaraujo1440 <162705599+marcelaraujo1440@users.noreply.github.com>
2024-11-13 19:34:20 -0300

    pagina sobre nos

--- paginas/sobre_nos.php
+++ paginas/sobre_nos.php
@@ -1,3 +1,30 @@
-<h1>Sobre Nós</h1>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/sobrenos.css">
+    <title>Sobre Nós - Coração Solidário</title>
+</head>
+<body>
+    <section class="sobre-nos">
+        <div class="texto">
+            <h2>Sobre nós</h2>
+            <p class="texto-bio">Nosso projeto nasceu com o objetivo de facilitar doações de roupas, conectando quem deseja ajudar a quem mais precisa. 
+                Criamos uma plataforma acessível e intuitiva para simplificar esse processo, ajudando tanto as pessoas que querem doar, mas não sabem onde,
+                quanto aquelas que buscam ajuda e enfrentam dificuldades em encontrar doações de forma prática e direta.
+            </p>
+            <p class="texto-bio">Além de conectar doadores e pessoas em situação de necessidade, nossa iniciativa se preocupa com a conscientização sobre o impacto positivo da reutilização de roupas, 
+                contribuindo para um consumo mais sustentável e consciente. Queremos promover uma rede de solidariedade,
+                onde cada peça de roupa ganha uma nova vida e leva um pouco de conforto a quem recebe.</p>
+        </div>
+        <div class="imagem">
+            <img src="assets/logo coracao.png" alt="Imagem de exemplo">
+        </div>
+    </section>
 
-<button><a href="home.php?dir=paginas&&file=feedback">Faça uma avaliação</a></button>
+    <div class="feedback">
+        <a class="feedback-button" href="home.php?dir=paginas&&file=feedback">Faça uma avaliação</a>
+    </div>
+</body>
+</html>
